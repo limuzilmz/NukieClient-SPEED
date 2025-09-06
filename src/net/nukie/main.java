86936c7492f0c869035fde85b0c8f5f0471baac7
Leset <3462503091@qq.com>
2025-09-06 15:16:33 +0800

    1

---
+++ src/net/nukie/main.java
@@ -0,0 +1,12 @@
+package net.nukie;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class main {
+    private static final Logger logger = LogManager.getLogger();
+    public static double Version = 0.1 ;
+    public static void nukie() {
+        logger.info("Nukie Client Loaded");
+    }
+}
